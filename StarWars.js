5e9f25ad5c66bcdb96e755913fc0dfdfb9873a2a
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-10-21 15:09:29 -0500

    StarWars script from html to js and created strings

---
+++ StarWars.js
@@ -0,0 +1,8 @@
+const starwars = {
+    image: 'the mandalorian and baby yoda',
+    title: 'Special Look | The Mandalorian | Disney+',
+    length: '1:00',
+    user: 'Star Wars',
+    views: '1M',
+    upoloaded: '1 day ago'
+}
